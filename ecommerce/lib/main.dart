46244ceccc886b7c9bd4ecb4c1517222e7f9ddf3
Matheus-2700 <mkarvat10@gmail.com>
2024-09-04 20:57:08 -0300

    atualizando

--- ecommerce/lib/main.dart
+++ ecommerce/lib/main.dart
@@ -1,3 +1,5 @@
+// lib/main.dart
+import 'package:ecommerce/screen/homeScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,113 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainScreen(),
-    );
-  }
-}
-
-class MainScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Git ADD '),
-        backgroundColor: Colors.white,
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search, color: Colors.black),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.notifications, color: Colors.black),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomeScreen()),
-            );
-          },
-          child: Text('Go to HomeScreen'),
-        ),
-      ),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen'),
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(16.0),
-        children: [
-          // Card for Discover Page
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: Container(
-              height: 150,
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Discover', style: TextStyle(fontSize: 24)),
-                  // Content goes here
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 16.0),
-
-          // Card for Categories Page
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: Container(
-              height: 150,
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Categories', style: TextStyle(fontSize: 24)),
-                  // Content goes here
-                ],
-              ),
-            ),
-          ),
-
-          // Card for Product Details
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: Container(
-              height: 150,
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Balloon Chair', style: TextStyle(fontSize: 24)),
-                  // Content goes here
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+      home: HomeScreen(),
     );
   }
 }
--- ecommerce/lib/screen/homeScreen.dart
+++ ecommerce/lib/screen/homeScreen.dart
@@ -1,15 +1,88 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class HomeScreen extends StatelessWidget {
+  final List<String> images = [
+    'assets/images/beijinho.jpg',
+    'assets/images/brigadeiro.jpg',
+    'assets/images/casadinho.jpg',
+    'assets/images/coco.jpg',
+    'assets/images/damasco.jpg',
+    'assets/images/nutela.jpg',
+  ];
 
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
+  final List<double> imageHeights = [100, 160, 150, 120, 180, 160];
+  final List<double> imageWidths = [100, 160, 150, 120, 180, 160];
 
-class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.brown,
+      appBar: AppBar(
+        title: const Text('DOCES'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 230, 85, 131),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(1.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: Text(
+                'Bem vindo!!!',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Center(
+              child: Text(
+                'Escolha seus doces favoritos',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 3,
+                crossAxisSpacing: 2,
+                mainAxisSpacing: 2,
+                children: List.generate(images.length, (index) {
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: imageHeights[index],
+                          width: imageWidths[index],
+                          child: Image.asset(
+                            images[index],
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                      ],
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.calendar_month),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
   }
 }
